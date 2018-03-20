3f5c98cdf3ba8752388fffa39b12a45ba1d5e431
alexlwz <li@126.com>
2018-03-20 20:15:39 +0800

    add accountcontrol

---
+++ src/main/java/com/fcloud/CICDServer/Controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.fcloud.CICDServer.Controller;
+
+import com.fcloud.CICDServer.dataBean.AccountInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by user on 2018/3/20.
+ */
+public class AccountController {
+
+    @Autowired
+    AccountInfo accountInfo;
+
+}
